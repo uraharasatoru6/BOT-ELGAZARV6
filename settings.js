c36039cda879ae5e181054fc06c36bbf91785ea7
uraharasatoru6 <131049665+uraharasatoru6@users.noreply.github.com>
2023-04-18 19:45:03 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -24,12 +24,12 @@ global.location = "Africa, Egypt, Cairo" //ur location
 //bot bomdy 
 global.ownernomer = "201098906252" //ur number
 global.premium = ['201098906252'] //ur premium number
-global.botname = '❤️ᗴᒪᘜᗩᘔᗩᖇ ᗷOT❤️' //ur bot name
-global.linkz = "https://youtube.com/@ABDALLAH_MOHAMED" //your theme url which will be displayed on whatsapp
-global.websitex = "https://telegra.ph/file/ec763cc3e7c88d0e67682.jpg" //ur website to be displayed
-global.botscript = 'https://youtube.com/@ABDALLAH_MOHAMED' //script link
+global.botname = 'Urahara kuiske ❤️' //ur bot name
+global.linkz = "urahara kuiske" //your theme url which will be displayed on whatsapp
+global.websitex = "https://telegra.ph/file/5e06f1ccaf02573c985d2.jpg" //ur website to be displayed
+global.botscript = 'urahara' //script link
 global.themeemoji = "♥" //ur theme emoji
-global.packname = "⍣الجزار الوزير يروحي⍣" //ur sticker watermark packname
+global.packname = "⍣اوراهارا كيسكي يروحي⍣" //ur sticker watermark packname
 global.author = "⍣01098906252⍣" //ur sticker watermark author
 global.wm = "❤️ᗴᒪᘜᗩᘔᗩᖇ ᗷOT❤️." //ur watermark
 
